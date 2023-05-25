252667bedf504a4d85973b636985a8026dfff313
Lardzzz <aku.ikan04@gmail.com>
2023-05-25 18:05:37 +0700

    membuat kondisi untuk nomor yang terduplikasi

--- Doublelinkedlist/Doublelinkedlist.cpp
+++ Doublelinkedlist/Doublelinkedlist.cpp
@@ -41,3 +41,15 @@ void Doublelinkedlist::addNode() {
 	Node* newNode = new Node();
 	newNode->noMhs = nim;
 	newNode->name = nm;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+		}
+		newNode->next = START;
+		if (START != NULL)
+			START->prev = newNode;
+		newNode->prev = NULL;
+		START = newNode;
+		return;
+	}
